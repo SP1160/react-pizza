faa722ece086b95af7e5f607cfe2565828fcf2d2
Kirill <kir.esin2011@yandex.ru>
2023-07-01 01:58:34 +0300

    lesson 5 - create popup and add key

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -9,6 +9,7 @@ function Categories() {
             <ul>
                 {categories.map((category, index) => (
                     <li
+                        key={index}
                         onClick={() => setActiveIndex(index)}
                         className={activeIndex === index ? 'active' : ''}>
                         {category}
--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -14,6 +14,7 @@ function PizzaBlock({ title, price, imageUrl, sizes, types }) {
                 <ul>
                     {types.map((typeId) => (
                         <li
+                            key={typeId}
                             onClick={() => setActiveType(typeId)}
                             className={activeType === typeId ? 'active' : ''}>
                             {typeNames[typeId]}
@@ -23,6 +24,7 @@ function PizzaBlock({ title, price, imageUrl, sizes, types }) {
                 <ul>
                     {sizes.map((size, index) => (
                         <li
+                            key={size}
                             onClick={() => setActiveSize(index)}
                             className={activeSize === index ? 'active' : ''}>
                             {size} см.

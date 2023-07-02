4b48d1b98e734b29a8c76b9b4f1580b3b0a5953e
Kirill <kir.esin2011@yandex.ru>
2023-07-03 00:55:38 +0300

    lesson 6 - getting pizza from backend (mockapi), useEffect

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
 import './scss/app.scss';
-import pizzas from './assets/pizza.json';
 
 function App() {
+    const [items, setItems] = React.useState([]);
+
+    React.useEffect(() => {
+        fetch('https://64a1e9640079ce56e2db7a33.mockapi.io/items')
+            .then((response) => response.json())
+            .then((data) => setItems(data));
+    }, []);
+
     return (
         <div className="wrapper">
             <Header />
@@ -18,7 +25,7 @@ function App() {
                     </div>
                     <h2 className="content__title">Все пиццы</h2>
                     <div className="content__items">
-                        {pizzas.map((obj) => (
+                        {items.map((obj) => (
                             <PizzaBlock key={obj.id} {...obj} />
                         ))}
                     </div>

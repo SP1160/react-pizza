786ec3c2caaefe3882f0071761116a39dfb24e94
Kirill <kir.esin2011@yandex.ru>
2023-07-17 00:02:49 +0300

    lesson 15 - save filtering parameters in URL using qs library + use useNavigate hook

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 
 import { setSort } from '../redux/slices/filterSlice';
 
-const list = [
+export const sortList = [
     { name: 'популярности (DESC)', sortProperty: 'rating' },
     { name: 'популярности (ASC)', sortProperty: '-rating' },
     { name: 'цене (DESC)', sortProperty: 'price' },
@@ -48,7 +48,7 @@ function Sort() {
             {isOpen && (
                 <div className="sort__popup">
                     <ul>
-                        {list.map((obj, index) => (
+                        {sortList.map((obj, index) => (
                             <li
                                 key={index}
                                 onClick={() => onClickListItem(obj)}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
-import axios from 'axios';
+import qs from 'qs';
 import { useSelector, useDispatch } from 'react-redux';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-import { setCategoryId, setCurrentPage } from '../redux/slices/filterSlice';
-
+import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
-import Categories from '../components/Categories';
 import Pagination from '../components/Pagination';
 import { SearchContext } from '../App';
+import { setCategoryId, setCurrentPage, setFilters } from '../redux/slices/filterSlice';
+import { sortList } from '../components/Sort';
 
-const Home = () => {
-    const { categoryId, sort, currentPage } = useSelector((state) => state.filter);
+function Home() {
+    const navigate = useNavigate();
     const dispatch = useDispatch();
-
+    const isSearch = React.useRef(false);
+    const isMounted = React.useRef(false);
+    const { categoryId, sort, currentPage } = useSelector((state) => state.filter);
+    const { searchValue } = React.useContext(SearchContext);
     const [items, setItems] = React.useState([]);
     const [isLoading, setIsLoading] = React.useState(true);
-
     const onChangeCategory = (id) => {
         dispatch(setCategoryId(id));
     };
@@ -26,13 +30,11 @@ const Home = () => {
         dispatch(setCurrentPage(number));
     };
 
-    const { searchValue } = React.useContext(SearchContext);
-
-    React.useEffect(() => {
+    const fetchPizzas = () => {
         setIsLoading(true);
 
-        const order = sort.sortProperty.includes('-') ? 'asc' : 'desc';
         const sortBy = sort.sortProperty.replace('-', '');
+        const order = sort.sortProperty.includes('-') ? 'asc' : 'desc';
         const category = categoryId > 0 ? `category=${categoryId}` : '';
         const search = searchValue ? `&search=${searchValue}` : '';
 
@@ -44,8 +46,44 @@ const Home = () => {
                 setItems(res.data);
                 setIsLoading(false);
             });
+    };
 
+    React.useEffect(() => {
+        if (isMounted.current) {
+            const queryString = qs.stringify({
+                sortProperty: sort.sortProperty,
+                categoryId,
+                currentPage,
+            });
+            navigate(`?${queryString}`);
+        }
+        isMounted.current = true;
+    }, [categoryId, sort.sortProperty, currentPage]);
+
+    React.useEffect(() => {
+        if (window.location.search) {
+            const params = qs.parse(window.location.search.substring(1));
+
+            const sort = sortList.find((obj) => obj.sortProperty === params.sortProperty);
+
+            dispatch(
+                setFilters({
+                    ...params,
+                    sort,
+                }),
+            );
+            isSearch.current = true;
+        }
+    }, []);
+
+    React.useEffect(() => {
         window.scrollTo(0, 0);
+
+        if (!isSearch.current) {
+            fetchPizzas();
+        }
+
+        isSearch.current = false;
     }, [categoryId, sort.sortProperty, searchValue, currentPage]);
 
     const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
@@ -62,6 +100,6 @@ const Home = () => {
             <Pagination currentPage={currentPage} onChangePage={onChangePage} />
         </div>
     );
-};
+}
 
 export default Home;

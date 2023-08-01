31629fd238fc2b95b3c1a807c35a830397471805
Kirill <kir.esin2011@yandex.ru>
2023-08-02 01:50:01 +0300

    lesson 21 - add TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import Header from './components/Header';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 import FullPizza from './pages/FullPizza';
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import qs from 'qs';
 import { useSelector, useDispatch } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import Categories from '../components/Categories';
 import Sort from '../components/Sort';

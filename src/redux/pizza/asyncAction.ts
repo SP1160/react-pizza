209f4c57629908bdb587fc9bfe1cb883253085a3
kirill <kirillesin75@gmail.com>
2026-04-17 01:10:39 +0300

    fix api

--- src/redux/pizza/asyncAction.ts
+++ src/redux/pizza/asyncAction.ts
@@ -1,14 +1,14 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import { TPizza, TSearchPizzaParams } from './types';
-import axios from 'axios';
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { TPizza, TSearchPizzaParams } from "./types";
+import axios from "axios";
 
 export const fetchPizzas = createAsyncThunk<TPizza[], TSearchPizzaParams>(
-    'pizza/fetchPizzaStatus',
-    async (params) => {
-        const { sortBy, order, category, search, currentPage } = params;
-        const { data } = await axios.get<TPizza[]>(
-            `https://64a1e9640079ce56e2db7a33.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
-        );
-        return data;
-    },
+  "pizza/fetchPizzaStatus",
+  async (params) => {
+    const { sortBy, order, category, search, currentPage } = params;
+    const { data } = await axios.get<TPizza[]>(
+      `https://dummyjson.com/c/5097-6e37-452b-87b6?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
+    );
+    return data;
+  },
 );
